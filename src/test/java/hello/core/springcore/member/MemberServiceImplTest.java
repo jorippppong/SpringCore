be4823c80fab6e1365276e9785e16ddf87aa5e3f
jorippppong <duelee75@gmail.com>
2024-03-19 01:26:29 +0900

    AppConfig.java 추가 -> OOP 적용 완료

--- src/test/java/hello/core/springcore/member/MemberServiceImplTest.java
+++ src/test/java/hello/core/springcore/member/MemberServiceImplTest.java
@@ -1,13 +1,17 @@
 package hello.core.springcore.member;
 
 
+import hello.core.springcore.AppConfig;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
 
 class MemberServiceImplTest {
-    MemberService memberService = new MemberServiceImpl();
+    ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+    MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
     @Test
     void join(){
--- src/test/java/hello/core/springcore/order/OrderServiceImplTest.java
+++ src/test/java/hello/core/springcore/order/OrderServiceImplTest.java
@@ -1,17 +1,23 @@
 package hello.core.springcore.order;
 
+import hello.core.springcore.AppConfig;
 import hello.core.springcore.member.Grade;
 import hello.core.springcore.member.Member;
 import hello.core.springcore.member.MemberService;
 import hello.core.springcore.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
 
 class OrderServiceImplTest {
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServiceImpl();
+
+    ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+    OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
     @Test
     void createOrder() {
